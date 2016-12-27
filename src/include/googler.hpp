e1520d69f88acdca3865993bac4019874e8301a4
Charles <charlesbailey@protonmail.com>
2016-12-27 13:51:39 -0500

    document googler

--- src/include/googler.hpp
+++ src/include/googler.hpp
@@ -7,9 +7,12 @@
 namespace Googler {
 
 	/*
-		Formats num_res_to_show results into
+		Performs a google search and then formats the results.
 
-		title -> link
+		@param query the query to google for. It's URI encoded inside the function,
+			meaning that a human-readable query should be passed.
+		@param num_res_to_show the number of google results desired.
+		@res_vec a string vector in which the results are stored. "Title -> ShortenedLink"
 	*/
 	void do_google_search(const std::string& query,
 	                      const size_t& num_res_to_show,
