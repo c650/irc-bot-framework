182d89f6d3a129b3fd35d23284da9ba08ebee6ac
Charles <charlesbailey@protonmail.com>
2016-12-27 13:53:00 -0500

    document iplookup

--- src/include/iplookup.hpp
+++ src/include/iplookup.hpp
@@ -5,6 +5,12 @@
 
 namespace IPLookup {
 
+	/*
+		Looks up a host and returns a string concatenation of City, Region, Country.
+
+		@param host the host to look up. An IP address works best.
+		@return the string concatenation of the location.
+	*/
 	std::string do_lookup(const std::string& host);
 
 };
