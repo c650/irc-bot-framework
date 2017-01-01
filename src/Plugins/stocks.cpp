0088a9b823bd82b3e8c4999661cdf31435f5e6e5
Charles <charlesbailey@protonmail.com>
2017-01-01 16:32:59 -0500

    add exception handling to stocks plugin

--- src/Plugins/stocks.cpp
+++ src/Plugins/stocks.cpp
@@ -1,20 +1,26 @@
 #include "./include/http.hpp"
 #include "./include/json.hpp"
 
+#include <stdexcept>
+
 namespace Stocks {
 
 	std::string get_stock_summary(const std::string& q) {
 
 		std::string response = "";
 
-		if ( MyHTTP::get("https://www.google.com/finance/info?q=" + q.substr(0, q.find(" ")), response) ) {
-			response.erase(0, 5); // erase first five characters.
-			response.erase(response.length()-2); // and last two characters.
-			nlohmann::json data = nlohmann::json::parse(response);
+		try {
+			if ( MyHTTP::get("https://www.google.com/finance/info?q=" + MyHTTP::uri_encode(q.substr(0, q.find(" ")) ), response) ) {
+				response.erase(0, 5); // erase first five characters.
+				response.erase(response.length()-2); // and last two characters.
+				nlohmann::json data = nlohmann::json::parse(response);
 
-			return data["t"].get<std::string>() + " on " + data["e"].get<std::string>()
-					+ " was last at: $" + data["l"].get<std::string>()
-					+ ". Percent Day Change: " + data["cp"].get<std::string>();
+				return data["t"].get<std::string>() + " on " + data["e"].get<std::string>()
+						+ " was last at: $" + data["l"].get<std::string>()
+						+ ". Percent Day Change: " + data["cp"].get<std::string>();
+			}
+		} catch (std::exception& e) {
+			std::cerr << "Failed in get_stock_summary(): " << e.what() << '\n';
 		}
 
 		return "Failed to look up: " + q;
