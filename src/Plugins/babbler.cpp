f4f84eb716b4128c2eca5464766c81f37eff9591
Charles <charlesbailey@protonmail.com>
2017-01-25 20:28:52 -0500

    transfer babble to dynamic

---
+++ src/Plugins/babbler.cpp
@@ -0,0 +1,33 @@
+/*
+	babbler.cpp
+*/
+
+#include "../IRCBot/include/command-interface.hpp"
+#include "../IRCBot/include/bot.hpp"
+
+#include "./include/environment.hpp"
+#include "./include/json.hpp"
+#include "./include/random-line-stream.hpp"
+
+class BabblerCommand : protected IRC::CommandInterface {
+
+	RandomLineStream rls;
+
+  public:
+
+	BabblerCommand()
+		: CommandInterface("@babble", "does a babble."), rls(ENVIRONMENT["babble"]["filepath"].get<std::string>()) {}
+
+	void run(const IRC::Packet& p) {
+		p.reply(rls.sample());
+	}
+
+};
+
+extern "C" {
+
+	IRC::CommandInterface* maker(IRC::Bot* b = nullptr) {
+		return (IRC::CommandInterface*)(new BabblerCommand);
+	}
+
+};
