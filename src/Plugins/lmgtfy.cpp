60b75088215ee944e962f3c6d242c0f745caceaa
Charles <charlesbailey@protonmail.com>
2017-02-04 18:00:40 -0500

    edit lmgtfy

--- src/Plugins/lmgtfy.cpp
+++ src/Plugins/lmgtfy.cpp
@@ -8,6 +8,8 @@
 
 #include "./include/http.hpp"
 
+#include <algorithm>
+
 class LMGTFYCommand : protected IRC::CommandInterface {
 
   public:
@@ -16,7 +18,17 @@ class LMGTFYCommand : protected IRC::CommandInterface {
 		: CommandInterface("@lmgtfy ", "mean way to tell ppl to google things.") {}
 
 	void run(const IRC::Packet& p) {
-		p.reply("http://lmgtfy.com/?q=" + MyHTTP::uri_encode( p.content.substr(this->trigger().length()) ) );
+		std::string query = p.content.substr(this->trigger().length());
+
+		auto it = std::find_if(query.begin(),query.end(), [](const char& c){
+			return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9';
+		});
+
+		if (it == query.end()) {
+			p.reply("You supplied no meaningful search!");
+		} else {
+			p.reply("http://lmgtfy.com/?q=" + MyHTTP::uri_encode( query ) );
+		}
 	}
 
 };
