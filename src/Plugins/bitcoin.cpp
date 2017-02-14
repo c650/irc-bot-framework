d0142b025a1033204d013d03ac73d7e472729e8e
Charles <charlesbailey@protonmail.com>
2017-02-13 20:04:16 -0500

    add bitcoin command

---
+++ src/Plugins/bitcoin.cpp
@@ -0,0 +1,42 @@
+/*
+	bitcoin.cpp
+
+	checks the price of bitcoin.
+*/
+
+#include "../IRCBot/include/command-interface.hpp"
+#include "../IRCBot/include/bot.hpp"
+#include "../IRCBot/include/packet.hpp"
+
+#include "./include/json.hpp"
+#include "./include/http.hpp"
+
+class BitcoinCommand : protected IRC::CommandInterface {
+
+  public:
+
+	BitcoinCommand() : IRC::CommandInterface("@btc", "gets the current bitcoin price") {}
+
+	void run(const IRC::Packet& p) {
+		try {
+			std::string response = "";
+			MyHTTP::get("http://api.coindesk.com/v1/bpi/currentprice.json", response);
+
+			std::cout << response << '\n';
+
+			nlohmann::json data = nlohmann::json::parse(response);
+
+			p.reply("$" + data["bpi"]["USD"]["rate"].get<std::string>() + " as of: " + data["time"]["updated"].get<std::string>());
+
+		} catch (std::exception& e) {
+			std::cerr << "Error in BitcoinCommand: " << e.what() << '\n';
+			p.reply("Sorry. I failed to fetch data.");
+		}
+	}
+};
+
+extern "C" {
+	IRC::CommandInterface* maker(IRC::Bot* b = nullptr) {
+		return (IRC::CommandInterface*)(new BitcoinCommand);
+	}
+};
