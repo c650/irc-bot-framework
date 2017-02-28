f5bbae318bf05243b1db5f72e4da48620ee669f8
Charles <c650@users.noreply.github.com>
2017-02-27 21:08:36 -0500

    add compilation script for plugins

---
+++ compile-plugin.rb
@@ -0,0 +1,15 @@
+#! /bin/env ruby
+
+if ARGV.empty?
+	puts "Usage: ./compile-plugin.rb [plugin_name] [--with-curl]"
+end
+
+plugin_name = ARGV[0]
+
+with_curl = ARGV.length >= 2 && ARGV[1] == "--with-curl"
+
+command = "g++ -Os --std=c++17 -fPIC -shared -rdynamic -o ./bin/#{plugin_name}.so ./src/Plugins/#{plugin_name}.cpp #{with_curl ? "./src/Plugins/http.cpp -lcurl" : ""}"
+
+puts command
+output = `#{command}`
+puts output unless output.empty?
