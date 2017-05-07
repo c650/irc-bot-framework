19ddcd8a77b045a2551feb1cdc1bfd793d76d9a1
Charles <c650@users.noreply.github.com>
2017-05-07 14:38:53 -0400

    edit to include any extra dependencies

--- compile-plugin.rb
+++ compile-plugin.rb
@@ -7,9 +7,15 @@ end
 
 plugin_name = ARGV[0]
 
-with_curl = ARGV.length >= 2 && ARGV[1] == "--with-curl"
+with_curl = ARGV.length >= 2 && ARGV.last == "--with-curl"
 
-command = "g++ -Os --std=c++17 -fPIC -shared -rdynamic -o ./bin/#{plugin_name}.so ./src/Plugins/#{plugin_name}.cpp #{with_curl ? "./src/Plugins/http.cpp -lcurl" : ""}"
+deps = ""
+if ARGV.length >= 2
+	deps = ARGV[1...ARGV.length-1].join(" ")
+	deps += ARGV.last unless with_curl
+end
+
+command = "g++ -Os --std=c++17 -fPIC -shared -rdynamic -o ./bin/#{plugin_name}.so ./src/Plugins/#{plugin_name}.cpp #{deps} #{with_curl ? "./src/Plugins/http.cpp -lcurl" : ""}"
 
 puts command
 output = `#{command}`
